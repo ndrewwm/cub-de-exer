2f01b424eb014b396aae24f76146b15673c22c3d
ndrewwm <moore.andrew09@gmail.com>
2022-11-20 22:35:09 -0700

    For discussion
    - trusting the primary affiliations listed in the where clause
      - the person_type case statement can be simpler if those values are solid

--- exercise-1-sql/query.sql
+++ exercise-1-sql/query.sql
@@ -1,33 +1,37 @@
 select distinct
        dp.dir_uid as username,
        de.mail as email,
+
+       -- rows w/ primary affiliations of 'Affiliate' and 'Member' are not retained 
+       -- in the set of results, so cases dedicated to these values are omitted;
        case
-          when dp.primaryaffiliation = 'Student'
-            or (daf.description like 'Student%' or daf.description = 'Continuing Ed Non-Credit Student')
-               then 'Student'
-          
-          when dp.primaryaffiliation in ('Staff', 'Officer/Professional', 'Employee', 'Faculty')
-            or (dp.primaryaffiliation = 'Member' and daf.description = 'Faculty')
-               then 'Faculty/Staff'
+         when dp.primaryaffiliation = 'Student'
+           or (dp.primaryaffiliation != 'Student' and 
+               daf.edupersonaffiliation in ('Employee', 'Faculty', 'Affiliate') and
+               daf.description like '%Student%')
+              then 'Student'
 
-          else 'Student'
-        end as person_type
-   from dirsvcs.dir_person dp 
-        inner join dirsvcs.dir_affiliation daf
-        on daf.uuid = dp.uuid
-           and daf.campus = 'Boulder Campus' 
-           and daf.description not like 'POI_%'
-           and daf.description not in ('Admitted Student', 'Alum', 'Confirmed Student', 'Former Student', 
-                                       'Member Spouse', 'Sponsored', 'Sponsored EFL', 'Retiree', 'Boulder3')
+         when dp.primaryaffiliation in ('Staff', 'Faculty', 'Officer/Professional')
+              then 'Faculty/Staff'
 
-        left join dirsvcs.dir_email de
-        on de.uuid = dp.uuid
-           and de.mail_flag = 'M'
-           and de.mail is not null
-  where dp.primaryaffiliation not in ('Not currently affiliated', 'Retiree', 'Affiliate', 'Member') -- likely that 'Affiliate' and 'Member' should be removed
-    and lower(de.mail) not like '%cu.edu' -- should this be in join clause?
-    and de.mail is not null               -- should this be in join clause? is it attempting to filter out rows that *have* a null mail, *after* the join?
-    and (
-      (dp.primaryaffiliation != 'Student' and lower(de.mail) not like '%cu.edu') or 
-      (dp.primaryaffiliation = 'Student' and exists (select 'x' from dirsvcs.dir_acad_career where uuid = dp.uuid))
-    )
+         else 'Student'
+       end as person_type
+  from dirsvcs.dir_person dp
+       inner join dirsvcs.dir_affiliation daf
+       on daf.uuid = dp.uuid
+          and daf.campus = 'Boulder Campus'
+          and daf.description not like 'POI_%'
+          and daf.description not in ('Admitted Student', 'Alum', 'Confirmed Student', 'Former Student',
+                                      'Member Spouse', 'Sponsored', 'Sponsored EFL', 'Retiree', 'Boulder3')
+
+       -- do not keep records with a null address, left --> inner join to simplify
+       inner join dirsvcs.dir_email de
+       on de.uuid = dp.uuid
+          and de.mail_flag = 'M'
+          and de.mail is not null
+          and lower(de.mail) not like '%cu.edu'
+ where dp.primaryaffiliation not in ('Not currently affiliated', 'Retiree', 'Affiliate', 'Member')
+   and (dp.primaryaffiliation != 'Student' or
+        (dp.primaryaffiliation = 'Student' and exists (select 'x'
+                                                         from dirsvcs.dir_acad_career dac
+                                                        where dac.uuid = dp.uuid)));
